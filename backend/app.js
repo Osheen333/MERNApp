599d130efb3182fcec48dd9c247f6150e3027ce7
Osheen Baby <osheeen333@gmail.com>
2023-02-24 19:31:17 -0500

    Changes

---
+++ backend/app.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const bcrypt = require("bcryptjs");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+const jwt = require("jsonwebtoken");
+var nodemailer = require("nodemailer");
+
+const JWT_SECRET =
+  "hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
+
+const mongoUrl =
+  "mongodb+srv://adarsh:adarsh@cluster0.zllye.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+
+require("./userDetails");
+
+const User = mongoose.model("UserInfo");
+app.post("/register", async (req, res) => {
+  
+    const { name, email, password,cpassword } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+     // return res.json({ error: "User Exists" });
+      return res.status(400).send("User already registered.");
+       
+
+    }
+    await User.create({
+      name,
+      
+      email,
+      password: encryptedPassword,
+      cpassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
+
+app.post("/login-user", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: "User Not found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email: user.email }, JWT_SECRET, {
+      expiresIn: "10s",
+    });
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+  res.json({ status: "error", error: "InvAlid Password" });
+});
+
+app.post("/userData", async (req, res) => {
+  const { token } = req.body;
+  try {
+    const user = jwt.verify(token, JWT_SECRET, (err, res) => {
+      if (err) {
+        return "token expired";
+      }
+      return res;
+    });
+    console.log(user);
+    if (user == "token expired") {
+      return res.send({ status: "error", data: "token expired" });
+    }
+
+    const useremail = user.email;
+    User.findOne({ email: useremail })
+      .then((data) => {
+        res.send({ status: "ok", data: data });
+      })
+      .catch((error) => {
+        res.send({ status: "error", data: error });
+      });
+  } catch (error) {}
+});
+
+
+app.listen(8000, () => {
+  console.log("Server Started");
+});
+
+
+
+
+
+
+
---
+++ backend/userDetails.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const UserDetailsScehma = new mongoose.Schema(
+  {
+    name:String,
+    email: { type: String, unique: true },
+    password: String,
+    cpassword:String,
+   
+  },
+  {
+    collection: "UserInfo",
+  }
+);
+
+mongoose.model("UserInfo", UserDetailsScehma);
